05998191d5c5f6a018a83706f8bc0efdfe8c898b
Roman Orlov <helltester666@gmail.com>
2022-03-18 18:02:14 +0100

    headless chrome

--- src/main/java/extensions/Driver.java
+++ src/main/java/extensions/Driver.java
@@ -3,6 +3,7 @@ package extensions;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class Driver {
 
@@ -11,7 +12,11 @@ public class Driver {
     public static WebDriver get() {
         if (driver == null) {
             WebDriverManager.chromedriver().setup();
-            driver = new ChromeDriver();
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--headless");
+            driver = new ChromeDriver(options);
         }
 
         return driver;
